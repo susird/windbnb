bdaf82f73ddce578b3a04dfeb204fbe4f36cfd81
susana <susir95@gmail.com>
2022-05-04 23:01:12 -0500

    search bar styles

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css'
 
 export const Header = () => {
+  const [toggle, setToggle] = useState(false)
+
 
   return (
-    <header className='header-container'>
-      <img className='logo-mage' src={process.env.PUBLIC_URL + "/images/logo.svg"} alt="logo"></img>
-      <form className='form-container'>
-        <input className='city-button' type="button" id='1' value="Helsinki, Finland" />
-        <input className='guests' type="button" id='2' value="Add guests" />
-        <input className='search' type="button" id='6' value="x" />
+    <>
+      <header className={`header-${!toggle ? "container" : "hidden"}`}>
+        <img className='logo-mage' src={process.env.PUBLIC_URL + "/images/logo.svg"} alt="logo"></img>
+        <div className='form-container'>
+          <button className='city-button' onClick={() => setToggle(!toggle)}>Helsinki, Finland</button>
+          <button className='guests'>Add guests</button>
+          <button className='search'>X</button>
+        </div>
+      </header>
+      {toggle && <form className='toggle-form'>
+        <div className='search-bar'>
+          <div className='location-container'>
+            <p className='location-title'>LOCATION</p>
+            <input value="Helsinki, Finland" className='location-open' />
+          </div>
+          <div className='guests-container'>
+            <p className='location-title'>GUESTS</p>
+            <input value="Add guests" className='guests-open' />
+          </div>
+          <div className='search-container'>
+            <img className='search-img-open' src={process.env.PUBLIC_URL + "/images/search-open.svg"} alt="search" />
+            <button onClick={() => setToggle(toggle)} className='search-open'>Search</button>
+          </div>
+        </div>
+        <ul>
+          <li>Helsinki, Finland</li>
+          <li>Turku, Finland</li>
+          <li>Oulu, Finland</li>
+          <li>Vaasa, Finland</li>
+        </ul>
       </form>
-    </header>
+      }
+    </>
   )
 
 }
