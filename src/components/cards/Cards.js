70feb733a275092af1d646503dd87047750853eb
susird <susir95@gmail.com>
2022-06-26 23:38:46 -0500

    added styles for description and img for score

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -7,10 +7,15 @@ export const Cards = ({ data }) => {
       {data && data.length ? data.map(card =>
         <article className="card-content" key={card.sku}>
           <img className="card-img" alt="houseImage" src={"/" + card.img.product} />
-          <h3 className="product-name">{card.title}</h3>
-          <p className="product-description">{card.description}</p>
-          <p className="beds">{card.beds}</p>
-          <p className="host-type">{card.host}</p>
+          <div className="details-container">
+            <div className="details-container-range">
+              <p className={card.host !== "" && "host-type"}>{card.host}</p>
+              <p className="product-description">{card.description}{card.beds !== "" && ' . ' + card.beds}</p>
+              <img className="card-img" alt="houseImage" src={"/" + card.score.img} />
+              <p className="score">{card.score.rating}</p>
+            </div>
+            <h3 className="product-name">{card.title}</h3>
+          </div>
         </article>
       ) : "loading..."}
     </article>
